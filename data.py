e36225879843886033f5a64ef3d3f8ee82a0f549
Ali Darwiche <darwiche2005@utexas.edu>
2016-10-18 18:52:36 -0500

    added States & Campgrounds to data.py

--- data.py
+++ data.py
@@ -17,12 +17,14 @@ class Parks(db.Model):
     closetime = db.Column(db.Integer)
     website = db.Column(db.String)
  
-    # event_id = db.Column(db.Integer, db.ForeignKey('event.idnum'))
-    # campground = db.Column(db.Integer, db.ForeignKey('Campgrounds.idnum'))
- 
-    event = db.relationship('Events', backref = 'Parks', lazy = 'dynamic')
-    # state = db.relationship('State', backref = 'ParksFK', lazy = 'dynamic')
-    # campgrounds = db.relationship('Campgrounds', backref = 'ParksFK', lazy = 'dynamic')
+    eventId = db.Column(db.Integer, db.ForeignKey('Events.idnum'))
+    events = db.relationship('Events', backref = 'Parks', lazy = 'dynamic')
+
+    
+    states = db.relationship('States', backref = 'Parks', lazy = 'dynamic')
+
+    campgroundId = db.Column(db.Integer, db.ForeignKey('Campgrounds.idnum'))
+    campgrounds = db.relationship('Campgrounds', backref = 'Parks', lazy = 'dynamic')
  
     def __init__(self, name, activities, campground, price, opentime, closetime, website):
         self.name = name
@@ -48,11 +50,11 @@ class Events(db.Model):
     city = db.Column(db.String)
     #activities = db.Column(db.String,foreign_key = True) #navigates to activity
  
-    park_id = db.Column(db.Integer, db.ForeignKey('Parks.idnum'))
-    # state = db.Column(db.Integer, db.ForeignKey('State.idnum'))
- 
-    # parks = db.relationship('Parks', backref = 'event', lazy = 'dynamic')
-    # state = db.relationship('State', backref = 'EventFK', lazy = 'dynamic')
+    parkId = db.Column(db.Integer, db.ForeignKey('Parks.idnum'))
+    parks = db.relationship('Parks', backref = 'Events', lazy = 'dynamic')
+
+    stateId = db.Column(db.Integer, db.ForeignKey('States.idnum'))
+    states = db.relationship('States', backref = 'Events', lazy = 'dynamic')
  
     def __init__(self, email, name, startDate, category, url, park, city, state):
         self.email = email
@@ -70,69 +72,71 @@ class Events(db.Model):
             return '<Event %r>' % self.name
  
 
-# class State(db.Model):
-#     __tablename__ = 'State'
-#     idnum = db.Column(db.Integer, primary_key = True)
-#     numparks = db.Column(db.Integer)
-#     name = db.Column(db.String)
-#     highestPoint = db.Column(db.String)
-#     population = db.Column(db.Integer)
-#     capital = db.Column(db.String)
-#     #largestPark = db.Column(db.String)
-#     #mostRecentEvent = db.Column(db.String)
-#     #highestPointNum
-#     #terrain
-#     #safety concerns
+class States(db.Model):
+    __tablename__ = 'States'
+    idnum = db.Column(db.Integer, primary_key = True)
+    numparks = db.Column(db.Integer)
+    name = db.Column(db.String)
+    highestPoint = db.Column(db.String)
+    population = db.Column(db.Integer)
+    capital = db.Column(db.String)
+    #largestPark = db.Column(db.String)
+    #mostRecentEvent = db.Column(db.String)
+    #highestPointNum
+    #terrain
+    #safety concerns
  
-#     largestParkId = db.Column(db.Integer, db.ForeignKey('Parks.idnum'))
-#     mostRecentEventId = db.Column(db.Integer, db.ForeignKey('Event.idnum'))
+    largestParkId = db.Column(db.Integer, db.ForeignKey('Parks.idnum'))
+
+    mostRecentEventId = db.Column(db.Integer, db.ForeignKey('Events.idnum'))
+    events = db.relationship('Events', backref = 'States', lazy = 'dynamic')
+
+    campgrounds = db.relationship('Campgrounds', backref = 'States', lazy = 'dynamic')
+ 
+    def __init__(self, name, capital, numparks, largestParkId, mostRecentEventId, highestPoint,population):
+        self.name = name
+        self.capital = captital
+        self.numparks = numparks
+        self.largestParkId = largestParkId
+        self.mostRecentEventId = mostRecentEventId
+        self.highestPoint = highestPoint
+        self.population = population  
+ 
+ 
    
-#     Event = db.relationship('Event', backref = 'StateFK', lazy = 'dynamic')
-#     Campgrounds = db.relationship('Campgrounds', backref = 'StateFK', lazy = 'dynamic')
+    def __repr__(self):
+            return '<State %r>' % self.name
  
-#     def __init__(self, name, capital, numparks, largestParkId, mostRecentEventId, highestPoint,population):
-#         self.name = name
-#         self.capital = captital
-#         self.numparks = numparks
-#         self.largestParkId = largestParkId
-#         self.mostRecentEventId = mostRecentEventId
-#         self.highestPoint = highestPoint
-#         self.population = population  
+class Campgrounds(db.Model):
+    __tablename__ = 'Campgrounds'
+    idnum = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    longitude = db.Column(db.Float)
+    latitude = db.Column(db.Float)
+    address = db.Column(db.Float)
+    price = db.Column(db.Float)
+    electricity = db.Column(db.Boolean)
+    water = db.Column(db.Boolean)
+    rv_access = db.Column(db.Boolean)
+    tents = db.Column(db.Boolean)
+
+    parkId = db.Column(db.Integer, db.ForeignKey('Parks.idnum'))
+    stateId = db.Column(db.Integer, db.ForeignKey('States.idnum'))
  
+    parks = db.relationship('Parks', backref = 'Campgrounds', lazy = 'dynamic')
  
+    def __init__(self, name, activities, longitude, latitude, address, price, electricity, water, rv_access, tents):
+        self.name = name
+        self.activities = activities
+        self.longitude = longitude
+        self.latitude = latitude
+        self.address = address
+        self.price = price
+        self.electricity = electricity
+        self.water = water
+        self.rv_access = rv_access
+        self.tents = tents
    
-#     def __repr__(self):
-#             return '<State %r>' % self.name
- 
-# class Campgrounds(db.Model):
-#     __tablename__ = 'Campgrounds'
-#     idnum = db.Column(db.Integer, primary_key = True)
-#     name = db.Column(db.String)
-#     longitude = db.Column(db.Float)
-#     latitude = db.Column(db.Float)
-#     address = db.Column(db.Float)
-#     price = db.Column(db.Float)
-#     electricity = db.Column(db.Boolean)
-#     water = db.Column(db.Boolean)
-#     rv_access = db.Column(db.Boolean)
-#     tents = db.Column(db.Boolean)
-#     park = db.Column(db.Integer, db.ForeignKey('Parks.idnum'))
-#     state = db.Column(db.Integer, db.ForeignKey('State.idnum'))
- 
-#     parks = db.relationship('Parks', backref = 'CampgroundsFK', lazy = 'dynamic')
- 
-#     def __init__(self, name, activities, longitude, latitude, address, price, electricity, water, rv_access, tents):
-#         self.name = name
-#         self.activities = activities
-#         self.longitude = longitude
-#         self.latitude = latitude
-#         self.address = address
-#         self.price = price
-#         self.electricity = electricity
-#         self.water = water
-#         self.rv_access = rv_access
-#         self.tents = tents
-   
-#     def __repr__(self):
-#             return '<Campgrounds %r>' % self.name
+    def __repr__(self):
+            return '<Campgrounds %r>' % self.name
  
