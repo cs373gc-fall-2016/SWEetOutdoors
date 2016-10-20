54d37b798a58eb128c559d75fe5ea9bb1c3f1c9b
Rambo Wu <rambo.wu95@gmail.com>
2016-10-20 11:02:50 -0500

    fixed campgrounds. changed models to use postgresql

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'
 db = SQLAlchemy(app)
 
 #pylint:disable=invalid-name, too-many-arguments, too-few-public-methods, too-many-instance-attributes
