a4669a032a4e37e5886781b250796ce8876b4a9b
Rambo Wu <RamboWu@wireless-10-147-40-255.public.utexas.edu>
2016-10-19 23:02:28 -0500

    changed init

--- app/__init__.py
+++ app/__init__.py
@@ -13,5 +13,21 @@ def texas():
 def california():
 	return render_template('/stateInstances/California.html')
 
+@app.route("/states/florida")
+def florida():
+	return render_template('/stateInstances/Florida.html')
+
+@app.route("/parks/BigBend")
+def bigBend():
+	return render_template('/parkInstances/BigBend.html')
+
+@app.route("/parks/DeathValley")
+def deathValley():
+	return render_template('/parkInstances/DeathValley.html')
+
+@app.route("/parks/Zilker")
+def zilker():
+	return render_template('/parkInstances/Zilker.html')
+
 if __name__ == "__main__":
 	app.run()
