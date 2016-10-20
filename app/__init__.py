ce879eeb99b2c2a264ea8df4a5df868c3a78bf05
Rambo Wu <RamboWu@wireless-10-147-40-255.public.utexas.edu>
2016-10-19 21:34:53 -0500

    Fixed stuff

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template('templates/index.html')
+	return render_template('index.html')
 
 @app.route("/texas")
 def texas():
-	return render_template('templates/texas.html')
+	return render_template('texas.html')
 
 if __name__ == "__main__":
 	app.run()
