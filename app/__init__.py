d668e1696c823878c4c04639d76afd278ddb3b1d
Rambo Wu <RamboWu@wireless-10-147-40-255.public.utexas.edu>
2016-10-19 21:24:20 -0500

    moved index fixed init

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template('index.html')
+	return render_template('templates/index.html')
 
 @app.route("/texas")
 def texas():
 	return render_template('templates/texas.html')
 
-if __name__ = "__main__":
+if __name__ == "__main__":
 	app.run()
