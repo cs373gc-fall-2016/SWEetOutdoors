fb9fd26ee6b56e7ef70c1a3b740017930d013b44
Elizabeth Haynes <ej2h@cs.utexas.edu>
2016-11-03 13:47:07 -0500

    may or may not have helped with anything, attempted to work with routing- states and parks and __init__

--- app/__init__.py
+++ app/__init__.py
@@ -27,114 +27,157 @@ def states():
 	routes to states table page
 	"""
 	states = State.query.all()
-	# cur = db.session.execute('select * from States order by name desc')
-	# states = cur.fetchall()
 	return render_template('states.html', states=states)
 
-@application.route("/states/texas")
-def texas():
+@application.route("/states/:idnum")
+def state_instance(idnum):
 	"""
-	routes to texas page
+	routes to a specific state page
 	"""
-	return render_template('/stateInstances/Texas.html')
+	state_instance = State.query.filter(State.idnum == idnum)
+	return render_template('stateInstance.html', state_instance=state_instance)
 
-@application.route("/states/california")
-def california():
+@application.route("/parks/:idnum")
+def park_for_state(stateID):
 	"""
-	routes to california page
+	routes to a specific park page from a state
 	"""
-	return render_template('/stateInstances/California.html')
+	# state_instance = State.query.filter(State.name == stateID)
+	park_instance = Park.query.filter(Park.state_fk == stateID).one()
+	# park_idnum = state
+	return render_template('parkInstance.html', park_instance=park_instance)
 
-@application.route("/states/florida")
-def florida():
-	"""
-	routes to florida page
-	"""
-	return render_template('/stateInstances/Florida.html')
+# @application.route("/states/texas")
+# def texas():
+# 	"""
+# 	routes to texas page
+# 	"""
+# 	return render_template('/stateInstances/Texas.html')
+
+# @application.route("/states/california")
+# def california():
+# 	"""
+# 	routes to california page
+# 	"""
+# 	return render_template('/stateInstances/California.html')
+
+# @application.route("/states/florida")
+# def florida():
+# 	"""
+# 	routes to florida page
+# 	"""
+# 	return render_template('/stateInstances/Florida.html')
 
 @application.route("/parks")
 def parks():
 	"""
 	routes to parks table page
 	"""
-	return render_template('parks.html')
+	parks = Park.query.all()
+	return render_template('parks.html', parks=parks)
 
-@application.route("/parks/BigBend")
-def bigBend():
+@application.route("/parks/:idnum")
+def park_instance():
 	"""
-	routes to Big Bend page
+	routes to a specific park page
 	"""
-	return render_template('/parkInstances/BigBend.html')
+	park_instance = Park.query.filter(Park.idnum == idnum)
+	return render_template('parkInstance.html', park_instance=park_instance)
 
-@application.route("/parks/DeathValley")
-def deathValley():
-	"""
-	routes to Death Valley page
-	"""
-	return render_template('/parkInstances/DeathValley.html')
+# @application.route("/parks/BigBend")
+# def bigBend():
+# 	"""
+# 	routes to Big Bend page
+# 	"""
+# 	return render_template('/parkInstances/BigBend.html')
 
-@application.route("/parks/Zilker")
-def zilker():
-	"""
-	routes to Zilker Park page
-	"""
-	return render_template('/parkInstances/Zilker.html')
+# @application.route("/parks/DeathValley")
+# def deathValley():
+# 	"""
+# 	routes to Death Valley page
+# 	"""
+# 	return render_template('/parkInstances/DeathValley.html')
+
+# @application.route("/parks/Zilker")
+# def zilker():
+# 	"""
+# 	routes to Zilker Park page
+# 	"""
+# 	return render_template('/parkInstances/Zilker.html')
 
 @application.route("/events")
 def events():
 	"""
 	routes to events table page
 	"""
-	return render_template('events.html')
+	events = event.query.all()
+	return render_template('events.html', events=events)
 
-@application.route("/events/AustinCityLimits")
-def austinCityLimits():
+@application.route("/events/:idnum")
+def event_instance():
 	"""
-	routes to Austin City Limits page
+	routes to a specific event page
 	"""
-	return render_template('/eventInstances/AustinCityLimits.html')
+	event_instance = event.query.filter(event.idnum == idnum)
+	return render_template('eventInstance.html', event_instance=event_instance)
 
-@application.route("/events/YellowStoneAnniversary")
-def yellowStoneAnniversary():
-	"""
-	routes to YellowStoneAnniversary page
-	"""
-	return render_template('/eventInstances/YellowStoneAnniversary.html')
+# @application.route("/events/AustinCityLimits")
+# def austinCityLimits():
+# 	"""
+# 	routes to Austin City Limits page
+# 	"""
+# 	return render_template('/eventInstances/AustinCityLimits.html')
 
-@application.route("/events/RunForTheHungry")
-def runForTheHungry():
-	"""
-	routes to run for the hungry page
-	"""
-	return render_template('/eventInstances/RunForTheHungry.html')
+# @application.route("/events/YellowStoneAnniversary")
+# def yellowStoneAnniversary():
+# 	"""
+# 	routes to YellowStoneAnniversary page
+# 	"""
+# 	return render_template('/eventInstances/YellowStoneAnniversary.html')
+
+# @application.route("/events/RunForTheHungry")
+# def runForTheHungry():
+# 	"""
+# 	routes to run for the hungry page
+# 	"""
+# 	return render_template('/eventInstances/RunForTheHungry.html')
 
 @application.route("/campgrounds")
 def campgrounds():
 	"""
 	routes to campgrounds table page
 	"""
-	return render_template('campgrounds.html')
+	campgrounds = campground.query.all()
+	return render_template('campgrounds.html', campgrounds=campgrounds)
 
-@application.route("/campgrounds/BearCreek")
-def bearCreek():
+@application.route("/campgrounds/:idnum")
+def campground_instance():
 	"""
-	routes to Bear Creek page
+	routes to a specific campground page
 	"""
-	return render_template('/campgroundInstances/BearCreek.html')
+	campground_instance = campground.query.filter(campground.idnum == idnum)
+	return render_template('campgroundInstance.html', campground_instance=campground_instance)
 
-@application.route("/campgrounds/Everglades")
-def everglades():
-	"""
-	routes to Everglades page
-	"""
-	return render_template('/campgroundInstances/Everglades.html')
+# @application.route("/campgrounds/BearCreek")
+# def bearCreek():
+# 	"""
+# 	routes to Bear Creek page
+# 	"""
+# 	return render_template('/campgroundInstances/BearCreek.html')
 
-@application.route("/campgrounds/YosemiteCampground")
-def yosemiteCampground():
-	"""
-	routes to Yosemite campgrounds page
-	"""
-	return render_template('/campgroundInstances/YosemiteCampground.html')
+# @application.route("/campgrounds/Everglades")
+# def everglades():
+# 	"""
+# 	routes to Everglades page
+# 	"""
+# 	return render_template('/campgroundInstances/Everglades.html')
+
+# @application.route("/campgrounds/YosemiteCampground")
+# def yosemiteCampground():
+# 	"""
+# 	routes to Yosemite campgrounds page
+# 	"""
+# 	return render_template('/campgroundInstances/YosemiteCampground.html')
 
 if __name__ == '__main__':
 	application.debug = True
