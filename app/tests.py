aae8d95e6941f3de72206c31de200900e91b8a95
Matthew Rosenbaum <matthew.rosenbaum@utexas.edu>
2016-11-03 16:42:23 -0500

    sdfdfgsdfgvs

--- app/tests.py
+++ app/tests.py
@@ -53,8 +53,8 @@ class TestModels(TestCase):
         # # self.campground3 = campground3
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        #db.session.remove()
+        #db.drop_all()
 
     # def test_add(self):
     #     try:
@@ -114,7 +114,7 @@ class TestModels(TestCase):
 
     def test_state(self):
         sdict = {'name':'Texas','highest_point':'GuadalupePeak','population':'10000000','description':'big','total_area':'1234567'}
-        self.assertEqual(self.state.dictionary(),sdict)
+        self.assertEqual(self.state().dictionary(),sdict)
 
     # def test_campground(self):
     # 	cdict = {name:'BearCreek','latitude':'324.55','longitude':'367.65','electricity':'True',
