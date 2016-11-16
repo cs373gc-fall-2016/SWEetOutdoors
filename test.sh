f5bf2c1da95646ee419d3ddd632494460fb76051
Matthew Rosenbaum <matthew.rosenbaum@utexas.edu>
2016-11-16 13:54:59 -0600

    updating coverage again

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-coverage run app/tests.py >  app/tests.tmp 2>&1
+coverage run --branch --include=app/* app/tests.py >  app/tests.tmp 2>&1
 coverage report -m >> app/tests.out
 cat app/tests.out
 rm -f tests.tmp
